8715889312dd62221c808836b0cf6cb62d6014e2
ffrezende <fabriciofraga15@gmail.com>
2019-07-17 22:02:08 -0300

    Query Exemples

--- src/graph/Repositorio/GraphQLRepositorio.ts
+++ src/graph/Repositorio/GraphQLRepositorio.ts
@@ -3,5 +3,21 @@ import IGraphQLRepositorio from './IGraphQLRepositorio';
 export const root = {
   hello: () => {
     return 'Hello world!';
+  },
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
+  rollDice: ({ numDice, numSides }) => {
+    let output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   }
 };
--- src/graph/Repositorio/GraphSchema.ts
+++ src/graph/Repositorio/GraphSchema.ts
@@ -3,5 +3,9 @@ var { buildSchema } = require('graphql');
 export default buildSchema(`
   type Query {
     hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
